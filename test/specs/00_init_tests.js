7ab90240f8e8f99721f86f7079f3cbb369cf4183
Perry Mitchell <perry@perrymitchell.net>
2014-11-08 11:57:09 +0200

    Test failure

--- test/specs/00_init_tests.js
+++ test/specs/00_init_tests.js
@@ -3,6 +3,7 @@ describe("JSONSchema Initialisation", function() {
 	it("builds namespace", function() {
 		expect(window.JSONSchemaDoc).toBeDefined();
 		expect(typeof window.JSONSchemaDoc).toBe("object");
+		expect(false).toBe(true);
 	});
 
 });
